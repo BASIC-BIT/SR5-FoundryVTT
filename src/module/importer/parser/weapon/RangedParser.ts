873c2590c4ce7a2a751898e9706ed2afe3064271
BASIC-BIT <basic.bit.1001@gmail.com>
2023-10-01 03:33:05 -0400

    Refactor damage parsing logic into shared class

--- src/module/importer/parser/weapon/RangedParser.ts
+++ src/module/importer/parser/weapon/RangedParser.ts
@@ -1,40 +1,9 @@
 import { ImportHelper } from '../../helper/ImportHelper';
 import { WeaponParserBase } from './WeaponParserBase';
 import { Constants } from '../../importer/Constants';
-import DamageData = Shadowrun.DamageData;
-import DamageType = Shadowrun.DamageType;
-import {DataDefaults} from "../../../data/DataDefaults";
 import WeaponItemData = Shadowrun.WeaponItemData;
 
 export class RangedParser extends WeaponParserBase {
-    public GetDamage(jsonData: object): DamageData {
-        let jsonDamage = ImportHelper.StringValue(jsonData, 'damage');
-        let damageCode = jsonDamage.match(/[0-9]+[PS]/g)?.[0];
-
-        if (damageCode == null) {
-            return DataDefaults.damageData();
-        }
-
-        let damageType = damageCode.includes('P') ? 'physical' : 'stun';
-        let damageAmount = parseInt(damageCode.replace(damageType[0].toUpperCase(), ''));
-        let damageAp = ImportHelper.IntValue(jsonData, 'ap', 0);
-
-        const partialDamageData = {
-            type: {
-                base: damageType as DamageType,
-                value: damageType as DamageType,
-            },
-            base: damageAmount,
-            value: damageAmount,
-            ap: {
-                base: damageAp,
-                value: damageAp,
-                mod: []
-            }
-        }
-        return DataDefaults.damageData(partialDamageData);
-    }
-
     protected GetAmmo(weaponJson: object) {
         let jsonAmmo = ImportHelper.StringValue(weaponJson, 'ammo');
         let match = jsonAmmo.match(/([0-9]+)/g)?.[0];
@@ -52,7 +21,6 @@ export class RangedParser extends WeaponParserBase {
             item.system.range.rc.base = 0;
             item.system.range.rc.value = 0;
         }
-        
 
         if (jsonData.hasOwnProperty('range')) {
             item.system.range.ranges = Constants.WEAPON_RANGES[ImportHelper.StringValue(jsonData, 'range')];
@@ -63,10 +31,14 @@ export class RangedParser extends WeaponParserBase {
         item.system.ammo.current.value = this.GetAmmo(jsonData);
         item.system.ammo.current.max = this.GetAmmo(jsonData);
 
-        item.system.range.modes.single_shot = ImportHelper.StringValue(jsonData, 'mode').includes('SS');
-        item.system.range.modes.semi_auto = ImportHelper.StringValue(jsonData, 'mode').includes('SA');
-        item.system.range.modes.burst_fire = ImportHelper.StringValue(jsonData, 'mode').includes('BF');
-        item.system.range.modes.full_auto = ImportHelper.StringValue(jsonData, 'mode').includes('FA');
+        const modeData = ImportHelper.StringValue(jsonData, 'mode');
+
+        item.system.range.modes = {
+            single_shot: modeData.includes('SS'),
+            semi_auto: modeData.includes('SA'),
+            burst_fire: modeData.includes('BF'),
+            full_auto: modeData.includes('FA'),
+        };
 
         return item;
     }
